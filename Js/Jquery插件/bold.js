cd0c88cac717d937e4eb55984781b940b9b840dc
mengp <mengp@tiup.us>
2017-01-01 20:39:43 +0800

    魔方

---
+++ Js/Jquery插件/bold.js
@@ -0,0 +1,32 @@
+(function($){
+	$.fn.extend({
+		"bold":function(){
+			return this.css({fontWeight:"bold"});
+		}
+	});
+})(juery);
+//调用bold;
+$(function(){
+	$("ele").bold();
+});
+(function($){
+	$.fn.extend({
+		"bold":function(){
+			return this.css({fontWeight:"bold"});
+		}
+	});
+}(juery);
+(function($){
+	$.fn.extend({
+		"bold":function(){
+			return this.css({fontWeight:"bold"});
+		}
+	});
+}(jquery);
+(function($){
+	$.fn.extend({
+		"bold":function(){
+			return this.css({fontWeight:"bold"});
+		}
+	});
+})(jquery);
